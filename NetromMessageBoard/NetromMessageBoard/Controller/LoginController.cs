0b3c03985f7ce950556c8d8c75495dfd5db5f61e
RO Alex <ro.alex@ymail.com>
2017-11-08 03:00:42 +0200

    Login refactoring

---
+++ NetromMessageBoard/NetromMessageBoard/Controller/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetromMessageBoard.Interfaces;
+using NetromMessageBoard.Repository;
+using NetromMessageBoard.Repository.Interfaces;
+
+namespace NetromMessageBoard.Controller
+{
+    public class LoginController
+    {
+        private readonly ILogin _login;
+
+        public LoginController(ILogin login)
+        {
+            _login = login;
+        }
+
+        public bool Validate()
+        {
+            return !string.IsNullOrEmpty(_login.User) && !string.IsNullOrEmpty(_login.Password);
+        }
+
+        public bool CheckCredentials()
+        {
+            var userRepository = UnitOfWork.Instance.GetRepository<IUserRepository>();
+            return (userRepository.CheckCredentials(_login.User, _login.Password));
+        }
+    }
+}
---
+++ NetromMessageBoard/NetromMessageBoard/Interfaces/ILogin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetromMessageBoard.Interfaces
+{
+    public interface ILogin
+    {
+        string User { get; set; }
+        string Password { get; set; }
+    }
+}
